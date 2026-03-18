893f7dfe5fdf5d24083b2fee1a2b2bac860c81c6
Sergiu Mocanu <sergiu.mocanu.dev@outlook.com>
2026-03-18 14:52:41 +0100

    add image prompts for CLIP model

--- src/webcam_cv/config.py
+++ src/webcam_cv/config.py
@@ -1,12 +1,12 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 import os
 import torch
 
 @dataclass
 class AppConfig:
-    model_type: str = 'dinov2'
-    model_size: str = 'base'
+    model_type: str = None
+    model_size: str = None
 
     normal_frames_target: int = 50
     anomaly_threshold: float = 0.12
@@ -15,3 +15,11 @@ class AppConfig:
     saved_photos_folder: str = os.path.join(str(Path.home()), 'com-vis-photos')
     device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
     gpu_name: str = str(torch.cuda.get_device_name(0)) if torch.cuda.is_available() else None
+
+    clip_prompts: list[str] = field(default_factory=lambda: [
+        'an empty desk',
+        'a desk with an object on it',
+        'a hand in front of the camera',
+        'a tool on a desk',
+        'a person in front of the camera'
+    ])
